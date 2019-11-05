e11e5f5b8ce63d7cbbb1d17565429ce454106f9d
dogac-c <55766282+dogac-c@users.noreply.github.com>
2019-11-06 00:57:32 +0300

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,21 @@
+import pandas as pd
+dr = pd.read_csv("Plaza_Coffee.txt", sep = ";", header = 0)
+print(dr)
+
+columns = ["Company","Payment"]
+print( dr.groupby(columns)["Quantity"].sum() )
+
+Companies = {"Deloite & Touche": {"Cash": 0, "Credit": 0},
+ "EY": {"Cash": 0, "Credit": 0},
+ "KPMG": {"Cash": 0, "Credit": 0},
+ "PWC": {"Cash": 0, "Credit": 0}}
+
+for i in range(len(dr)):
+    Names, payment, quantity = dr.iloc[i]["Company"], dr.iloc[i]["Payment"], dr.iloc[i]["Quantity"]
+    Companies[Names][payment] = quantity  
+
+for key,value in Companies.items():
+    Names = key
+    cashCount = value["Cash"]
+    creditCount = value["Credit"]
+    print("From {0} {1} people have bought stuff on discount and paid in cash, also assistants got {2} serving of coffee on credit".format(Names,cashCount,creditCount))
